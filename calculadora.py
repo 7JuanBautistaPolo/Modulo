06e7e17fbcbb6e41fca2be532addb338b6d32829
Juan B. Polo <104925304+7JuanBautistaPolo@users.noreply.github.com>
2024-03-20 14:11:15 -0500

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 13:51:43 2024
+
+@author: Bautista Polo
+"""
+
+def suma(a,b):
+    return a+b
+
+def resta(a,b):
+    return a-b
+
+def multiplicacion(a,b):
+    return a*b
+
+def division(a,b):
+    return a/b
+"""
+print(suma(5,3))
+print(resta(5,3))
+print(multiplicacion(5,3))
+print(division(5,3))
+"""
---
+++ principal.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 13:53:58 2024
+
+@author: Bautista Polo
+"""
+
+# Bloque importar modulos/librerias
+import calculadora
+
+# Bloque variables y constantes
+# Bloque funciones
+# Bloque programa principal
+
+print(calculadora.suma(5,3))
+print(calculadora.resta(5,3))
+print(calculadora.multiplicacion(5,3))
+print(calculadora.division(5,3))
